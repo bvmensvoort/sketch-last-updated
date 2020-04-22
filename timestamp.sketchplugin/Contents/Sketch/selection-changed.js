89c4525b7881cd012c9ef3c09c705c15d2e1ce26
Bram van Mensvoort <bram.van.mensvoort@cz.nl>
2020-04-22 21:24:42 +0200

    Enable [timestamp-image]

--- timestamp.sketchplugin/Contents/Sketch/selection-changed.js
+++ timestamp.sketchplugin/Contents/Sketch/selection-changed.js
@@ -21,7 +21,6 @@ var onSelectionChanged = function (context) {
     //capturing the date and formatting it
     var d = new Date();
     var date = d.getDate() + "-" + d.getMonth() + "-" + d.getFullYear();
-    var time = z(d.getHours()) + ":" + z(d.getMinutes());
 
     function z(object, targetLength = 2, padString = "0") {
         return (object + "").padStart(targetLength, padString);
@@ -54,16 +53,7 @@ var onSelectionChanged = function (context) {
         var image;
         var lastGenerated;
 
-        function getTimestampDate() {
-            return date;
-        }
-        function getTimestampTime() {
-            return time;
-        }
-        function getTimestamp() {
-            return date + " " + time;
-        }
-        function getTimestampImage(context) {
+        function getTimestampImage() {
             if (!image) { 
                 image = getImage(context); 
             }
@@ -71,9 +61,15 @@ var onSelectionChanged = function (context) {
         }
 
         var replacements = new Map([
-            ["[timestamp-date]", getTimestampDate],
-            ["[timestamp-time]", getTimestampTime],
-            ["[timestamp]", getTimestamp],
+            ["[timestamp]", () => date + " " + time],
+            ["[timestamp-date]", () => date],
+            ["[timestamp-time]", () => d.getHours() + ":" + z(d.getMinutes())],
+            ["[timestamp-year]", () => d.getFullYear()],
+            ["[timestamp-month]", () => d.getMonth()],
+            ["[timestamp-day]", () => d.getDate()],
+            ["[timestamp-hour]", () => d.getHours()],
+            ["[timestamp-minute]", () => z(d.getMinute())],
+            ["[timestamp-second]", () => z(d.getSecond())],
             ["[timestamp-image]", getTimestampImage]
         ]);
 
@@ -102,7 +98,6 @@ var onSelectionChanged = function (context) {
                     sublayer.overridePoints().forEach(function (overridePoint) {
                         // Some code how to set overrides: https://sketchplugins.com/d/385-viewing-all-overrides-for-a-symbol/7
                         if (overridePoint.layerName().toLowerCase() === replacementKey) {
-                            console.log(replacementKey);
                             if (replacementKey === "[timestamp-image]") {
                                 //renderImage(sublayer);
 
@@ -212,6 +207,7 @@ class Blockies {
         
         return imageData;
 
+        // Original Blockies code
         canvas.width = canvas.height = opts.size * opts.scale;
 
         const cc = canvas.getContext('2d');
@@ -237,134 +233,63 @@ class Blockies {
     }
 }
 
-function createIcon(opts) {
-    var canvas = document.createElement('canvas');
-
-    renderIcon(opts, canvas);
-
-    return canvas;
-}
-
 function getImage(context, artboard) {
-
-    base64Image = generateImage(context, artboard);
-
-    //var base64Image2 = "iVBORw0KGgoAAAANSUhEUgAAAFUAAAA2CAYAAAC2ldWuAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAVaADAAQAAAABAAAANgAAAADTPCIuAAADXUlEQVR4Ae3ab0/aQBwH8G9ZdRhFNmEs2aJmBjUjUSFRt2XJnuzhXogvSR/7FvYW9kDizP7hRJkKGP4OHH8UKKXt7rqcaZYsQ7eTlt4ll19pz8v1kx/tcScgihAQAkJACAgBISAEhIAQEAJCQAjcTEDq9882NzeNftu6vZ18HYC3ypvrNHdtW49r75zjjQtUDrgCVaByEODQpchUgcpBgEOXIlMFKgcBDl2KTBWoHAQ4dCkyVaByEODQpchUgcpBgEOXIlN5oJLF5/Xt7e0Qh75d26Xs8/ninU4HW1tbLaJwSmqa1BNSP5Aa39jYSEqSpJNjUfoUkJLJpLlNomkaKG673TZjs9lEvV6nnxsEddcwjNe76ip+GPegGHf77N6dza5Q/3T7qqqCAjcaDRPZxO7KqOhBFPWHJD6ABvFotvr9FdXamB23Wi1Uq9VftdZEWQuiRIBLeohk8Shr5tp4I1SrVq/XM3ErlQoqBLqiTpoZnNMeowOvtalrjv8Z1Sql6zpqtRoocKFYQl4NIqPN4Dt5RLhpf/u/olqB6YuvXC4jl8uh3NQI7jTOSO244CXHDdUKfHFxgXw+b2ZvrhvEqfYEVWPK2mSojm8FlYnR7C2VSshmszi7HMehtoBz/T67PDTxVlGZGpnzolgsIp1OI9uawGFvETUy/x2WMhBUhkdfbBQ3k8kg05rEEcncmu5nlx0bB4rK1BguzdzTdgAJ9SkUB0/HbIFqxaVZe5zJ46u6QGYMs46citkKleHSX2xkTQLpmoTPvWU0dB+75IhoS1QmVygUkPp2jENlGke9BbLGcIddsnW0NSqVows6qVQK6VITe90oWSWz/xTM9qgsJekCTmL/AF+UME60OXbaltExa3aBQADP1lfxYiqHtZE9yFBtCUoH5RhUOliv14tYLIbVGRmvRt/BL9XpaduVa/3Pvx1G7/F4MD8/D7+/jImDOD4pi8jos3YY2tUYHIfKRh4KhUD21zCeSGCifon9XoRdGnh01Nf/d62xsTHzcRCbOkdU/gjJJj8VHI1KkWVZRjQaRTTUwZr8nsxkB7/x63hUCkufs0tLS1h+JOH5SHzgM4OhQKWwZBsdkUgEK7PjeDm6A6+k0NMDKUODyvTC4TBW5gIkY3cwgi47favxJ1G1cSyCOX+iAAAAAElFTkSuQmCC";
-    //const base64Image = 'iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAIAAACRXR/mAAAAAXNSR0IArs4c6QAAEDBJREFUWAm1WftvXcdxnt09z/u+uqQoUS+KtGQ7kZ3EERIHjuKmseNUhg07QI0qjeE6iAsU6QMoiqLtj/0v+mvzDxQFDARuixZp3DYp6jiW7cS24lAyRYqkSN73ee5uvtlzSTNGIlc/ZHFxeM6e3Z1vvpmdmT0UpbWSuAlLhF/VcCPcnTBEprq3xANF6YarYtZvFQlJWEOUhAEG9/vryJKEJhvyQiLjKwaTh9WsMAVNLWGKryhU5PFsHoAO/uvxpcJQ4cDDATh+A3lAVjXcSIcNj77DgZkYXeH2ZhNn+lSLHlwB1w1ziwuSknxDDmWF4UC6EyWM5YGCnPYsU800dr3ME8PSB8IdLqbEzBbCxJwoYKCGJMahH7rw6rymJoVFQRB3oFm8mXHBt1W3cNrxW56LxiME1pv93BOMUjVM+3C92XIFj+T+Q4PwPOt0f6rJfIsxlqwmIWbgKl5NpTkTjVUxRjFanoBx3AGjfgjIdeDCtt+/p9KwCjNMLGQf9D4Cz3kTuIMnsd+gOQoAyYA+pt+RqHkV2K4UhFt4odMF7x21mkrNw+QM1kxRnlz1sB3QDPsj5ldKOz90QAT7PC9WIXdW8909TAa/xiJetcJsGefDhvVhuFgRI8Aet+rKd2xuvK12FZ5hRDcDN+h2PIFiNw4ysK9YXbaYm4RrIFNehxucKWQ5VVMQB8IgQAgLV8NfKIbFeCdiCcbEpoHEUgkj4HKQA2TYsARVwTdWQBckukEsAJMA0F2dEryiG4pVgBiW5NGujdxkiAf7IcmQBWNb2IREOgsEEGABOnBeU7BxKRSC1cMqQMZgMJvZqmwCmWy+Sj56MQEAK1CMHQ+u8QbWxniiRllOPhjdIzVhqTonnZAdkpiSSKjMKbO2lAL44Nw05gX9iLw5ogWyDQoHpLFYjURT6phUCw4HLM6UparYhg4MS2Mhksw0YFmmB8AYe3VlTIDvgQ9YUkaUblE8GK5+P/SnQSaLcqTlUNtBUvSnaWJtLVC9xVMXbJoORhuj0SaJvB52ArFgyjAOR36tM85F2Drjdz9BysNLGWAXwjb4SWWl4K3IogUrxg2wAGcWx6su8Cysx7xhDOsF1kfZ+/8+2vuP1N8NMqyQiygRnoabhn6jEc83mqfJa4q42RGJMDt5ftPaG4UlrXMzUrE+Mk3UdHwyHA3aZx715SKVUekBGazgNgGYgTjscWQF1wALPzxjI8h9Czry4DxomAfz7b79zhv/fGZpGAZ7PkUAWvqpESC1GQRHw/opqi3SJKIgoOaptm/KSZqmv5BiV8VFaP0873dr7fEkW3v7dsuvixOPUF73VAuxhoWANEACOZW5nFhgcpufQTNSiU0D78ZAYGTetql8//rr/1QT6/Ay38+oaGC+LkWpIxmeCsL7SS7TuMO+n8JtOpDjSSOM1larYEJyANP4nmwHtDFc2/15u9dsUfM8aU/IBjSD40EWBCpQtZ/osMtAFXqBqSILmDEQaA0Vtylcn1z93uD2a2dPat8WNEo0nN3EVjQ8tRDX7qfGeVJHARhUmSSVBpl3nkJZ94pkOJlON6W/43s+5amU3qluMdp6LXsrDD/fACxsZGF9xDMgg3jsNyeYhXtVUEAY2Ye1/0r2yaza9f9cXf1epz3g/WjnKZGqNzVF4Mljqn4/RWdJ1ws9ybFBjC8C6VupEGFVSI2FqFhJ07DUYz+ISafw76aaaI1d8d/htR4tXSZRx3IA4TCVBWgmqMVtH4R7OPRYUjGgWv4/P3xZyt04zJPxHsGH4x5ihLal8hrUWKR4AUFLQ+Wal9mpZOVlmkz1eASbiu6p7sK9BtTmMAhMnKfDzXqQRHJ44+qrVPSpBFYWDO/SXA0gr8+aSO1OSTaimkKgSqwSPjub3qPRj+z/fjfd+Je4NR7FMgl1ba7e6DZ2tp/oHjkp22eJ2qZUcFsRYosjxuVSY5/HLoo670WQE7Y0/7h7a7Upx7HItzfW5DjviOa4H7aOPyoefoFqn8mzThCzf5el9lB9uakyK0pg0tYrSqkihynvE9221360fvOtMEJKEKOdcSTiRnBsb1u1ewuy0SEvQsQRKpAeIpzS1qUORETsKGRL/oF6BCLhtZa7jWUyPVPUG4iouS6ScbsZrK/9lN75IeWbQTDmNAX/tiDMsQffaiEcu8SCzJ5rpDzkkFV695WN91+O/VuyqUF+01I9a1ByuttaoNZxUnXYzmqvtEgj0lqjOcUBEdepswIBEQfbShg5PeO3F3x9I12/KsdbLUzRqc62WhGtvv3yUrdHZ79I9oSmhocqlWExXR6yJApYBCM4vcYN8sng2t57P2jo3WZoy+EU0awJF84VqTk6ci/5KPqQLZHgQl/55IGhzOdsWK2J9ApP4gchcw6PyEZRk9qno9Ee9a8TjbjkS5Jmz8sHk72fvtqNj9Jij2wdaBALKpf3KLVIBTkAheT7cMT12z/+r2RtdXkuMoP+dGpa3Q6lU665u23qb1GEdZH+GoQyAQg4/2IythFcFCvDvTjicARCP676CN0aUy2mFjhWtDV1mSUu3t9u91Zev/p2l06vLH5BSVUy5bMt6FEA7hkQZz/En1bsR+1310b9d9cWaipLs+F6miOB1q8tye9rP9wRtxT0kLE2PvuC1VIUSpaC3QvP2ARsSI6nkmNhQEcno2m30fWmyc3XrprbtBBTkWfUqG98cHMYn1yeX8IsY7Sn/AJVgauJENOgJXKSb3VmUBTIaO5zTy+v7938wcuetrEcJOM+RSLPis1rP1m493hPTSwfbzBbaovSCvFBS/4BFjdOYhUsXtYUYrvRCgO9s7O1m0+pHcOJ4vHUJsnc7jS455Fnjjx0mXRoUcmCnf14hSoBVMCgpadQFvmkscWOnXrmO14/W/+/fz0RhaHXTMqiFnlQl4Z9dQR2ASsTRAUPnHM4xqkLcFwGwfZz5RxW9CAKGKUJ9BQBx/YpRsmciGEqrd/dGHvnPve1xceeI9XFWKWCfJIHseOK7cabCUqijEFXg2SPguPkHT/+7IutlQsj4SGINqI47ScCzruawC3LokB60ZSXJqWgLHTpCjKUWdgWQAw9BQRZLu2RkgSlUg1pfIPCEt110+z9fJp1P31x8YknqY4oj+3G3DM5+2xxpnAFF/OPjYAwwedT1aDmiZXn/6wf9HZRLSRlO2pNUM+ltTI3XhxDNkeVAIWPRo4ujeaTKedad6PAIUag3wITsuTme9RWVKN6ngdbNrKnzy9dfobO3EMyTjOM5eLAYUMhxQ1Ke66KZZxwUfZSGARP/hzNf/KzL/ztRtGJo5ZJsqDW+GA7H+wVpHyN+gAbTvHZAFsYjBrYnwy04sQmy5JwXNdIKaSa0/c2Uc/6tibSKGos/CLzPnvlJTr3GVSqeB8hr1TRiktVXg+Nayze14caIIH4KQ7pcp7OX3rspb/7YK+Qtblx6Zuo279FNCgVymko5hSCs4J8aAuX4yCPYhPKlYhsXogKOBc3r1O92cnKdi6ObEy8373yJ+q+hxNqD8E2KALN7JogGxxhJjcmyTUgQWVtUH9hGJqKqJ/A8xbogccuPv2td/cK0Z4TtabdpWQDpmv5SFbYzzigTbF8QDqwhVKoypGuc/I0nDVUJiy29kKPhqmi9sk31vOHnvp2dPEy0ZyhHrqYcADCCcFtmAN3YljVxsZrsKgs7MB1DwbHbcrAhN+mS888+NiVnak/mIpQ0wTJHR6DrMwmV67+Rt1WVbmwIjIkaMONMdPpYIOKBGGn9+Pre1/+5p/T558kMadNHbue8zPEoPko8dl5UDy5Zz4X4RENf5GI4FxwE34uygSWUGGRgY3m6eaTf3zinkd03o5UlI11vj1gRYUwZRlECHCl8JAEMR+pSfPBG5W+Tvq7WbpJvfqZta38k5cuy69doXCRdKwoCIsUm5glAR1KTmTPCgN3sRGBYsYgY3VwYdDYo9wMNGVeXNcalcXc4uXfn++dRrrJU9rdKZh3KXLYqxaXDAvRYd814Aegr9T9AS10Tq/f2Dt77sHFr19hjv2YUKwCP2sF+dYiReLQq9PSlAFOWa4Jy/Hw1ze8QpMuT+EGx4Ysy/b+/lymk9xMzj1wLJ5HuQFSQ2PLspWNUxPFBMeyE4rDOHu9TLfFq+pLfr37+B+8QCufwoGHwrb1OWshlwLPjBH3XQNnbnTAFdDYt+7cAAgD4CthGNZqtbPP/WnfdI1ojHdTSkWZoBgKUYzj+I7CRGZ8fotbfrFTGNvY3MkpqD3+1LN09jzyG/khnMhZ50Amm+/g4eDmTrAABe3DoVIGOHI9+I2Lz/5NXsyPN/JsI/MaR7M0ZQISG3EQknZMetopzcmfXc+95srDjz9NFy6SqlEhKIgRcPENCOZjLB+uzU+ActBxJ1g80cEyrlW0kVj2vvDcxd978fptOdzxaeqHnir11JO+LLFphK8DNe3eWDV74vjyl5878sUnOGxmOBG1kY6A7ZCFgAYhHtZgWIfbx8DC0AoZMFXg2FXLOfnVF5a+8vzqTVHcSGAX+LCHU7yMskSL+jztRmtvjR/46h/RV75BqkMljrVz1g8nCCiueC1mMPCnMiIjOwz4N8ICiBk9UA9p1DV2f+wylO+mtfSHf9E8/6U3Vyc0MX4YsilR2/tNRNo3X7/5wMNPzT/6NIpSfNI1fgM84WyLGAAgGRK2+0B1mJ6PEPYbYR2Q9KuTXdTE4UqHSAD3feuvB8c+sT0IKAmgBrs9dbZvTNKFlaPffImaR5Hy4XbG9xMXT/gDklPKJRIg5M3EDf1wykPQ7gQLyCrbHdCGG8upz0aNuMgkHVu59PxfvnGdppNe3DxGVEuGwTvr5uKLf0W9Jc0nZlk4w6HoD7xZWosUZVnqjAlNHCbn6zPDVjgPRLrHj78gwKI+ZiOIAqkspIF+85WfvPIPD12w48HO1mZn+dKL9KmvJ7odq8AUhfAbWPRApKOB64xf7UNxxSU2WhW37hRO3bCPXizSUpn5/H0Ux1P+FkjDW+nP/m3tze922rXO6d/x7ntCNy+MKYxMGsIySJ1oznzIZs5wFUhcK1sxJmSMCmlVnlbgPir7Ds8C3xq4/sdO1yHyD6qteDm60OiMx7VW6H36cfJOWAqBJc8t0mUVobik5uYYmcXPGSYHbuZLsz/Q4m6NCHZQchrFhlD4XoIAjy8f0DS7ReXUdk4MkfOMCiXlOK/7+LgGQA4T4tNBQGfygAF/QBKDqVDjpiqr7hqWO6EgmDlrVKcmTtFOT1TJXEYDJEo3Unxq2TcUy8UP8CuDVoDwcEDQvp15xKFe9/jxF2nx1YhFoCJGceo+NOCIiYKcD6cZ3I01RlGd8D9VHAy3KB7gMJiKHzAzSfCnCiwGOFvjaAiluN21b5FFRSW0xHdBXgwFnI+vmDiG4csRBIoExx3+ioaQwOaTqAvQKk5wHsI9K3AIcIWU+2ZZiMdU43nm/7eV+KaLio8/ucOBS9yhWlKFLvB5nKIAK1bbzUnHCXcfRwWlwgRZBzzN5OIFI0M3t7v2rWrab/t692z9thG59X8JjZB/N6F8uAYAAAAASUVORK5CYII='
+    const base64Image = generateImage(context, artboard);
     var imageData = NSData.alloc().initWithBase64EncodedString_options(base64Image, NSDataBase64DecodingIgnoreUnknownCharacters);
     var image = NSImage.alloc().initWithData(imageData);
 
     return image;
 
     function generateImage(context) {
-        var sketch = require('sketch');
-        var Style = sketch.Style;
-        var ShapePath = sketch.ShapePath;
-        var Rectangle = sketch.Rectangle;
-        var Slice = sketch.Slice;
-        var page = document.selectedPage;
-        page = sketch.getSelectedDocument().selectedPage;
+        const sketch = require('sketch');
+        const Style = sketch.Style;
+        const ShapePath = sketch.ShapePath;
+        const Rectangle = sketch.Rectangle;
+        const page = sketch.getSelectedDocument().selectedPage.sketchObject; // It is for temporary use to create the icon
 
+        // Use old API to be able to export later
+        // https://sketchplugins.com/d/432-insert-new-layer-underneath-other-layers/4
+        let group = MSLayerGroup.new();
+        group.setName("Blockie");
+        page.addLayers([group]);
 
-        let iconData = new Blockies().renderIcon({seed: "randString"});
-        // let iconData = generateIconData({ // All options are optional
-        //     seed: 'randstring', // seed used to generate icon data, default: random
-        //     color: '#dfe', // to manually specify the icon color, default: random
-        //     bgcolor: '#aaa', // choose a different background color, default: white
-        //     size: 15, // width/height of the icon in blocks, default: 10
-        //     scale: 3 // width/height of each block in pixels, default: 5
-        // });
-
+        const iconData = new Blockies().renderIcon({seed: "randString"});
         const iconWidth = Math.sqrt(iconData.length);
         const pixelSize = 10;
+        const colors = ['', "#c0ffee", "#dfedfe"];
 
-        var shapes = [];
-        console.log(iconData);
-        
-        for (var i=0; i<iconData.length && i<200; i++) {
-            var pixel = iconData[i];
+        for (let i=0; i<iconData.length && i<200; i++) {
+            const pixel = iconData[i];
             if (pixel===0) continue;
             
-
-            var colors = ['', "#c0ffee", "#dfedfe"];
             const row = Math.floor(i / iconWidth);
             const col = i % iconWidth;
 
-            shapes.push(new ShapePath({
-                name: "my shape"+i,
+            // Use new API where possible
+            new ShapePath({
+                name: "Pixel "+i,
                 frame: new Rectangle(pixelSize * row, pixelSize * col, pixelSize, pixelSize),
                 style: { fills: [{ color: colors[pixel], fillType: Style.FillType.Color }], borders: [] },
-                parent: page
-            }));
-        }
-        
-        let slice = new Slice({
-            name: "my slice",
-            frame: new Rectangle(0, 0, iconWidth*pixelSize, iconWidth*pixelSize),
-            parent: page
-        });
-        console.log(slice)
+                parent: group
+            });
+        }        
 
-        var result = getBase64ImageFromSlice(slice, context);
+        var result = getBase64ImageFromSlice(group, context);
 
-        // Clean up
+        // Clean up / remove all objects
+
 
         return result;
     }
-    //     let mySquare = new Shape({
-    //         parent: parentLayer, 
-    //         frame: { x: 53, y: 213, width: 122, height: 122 },
-    //         style: { fills: ['#35E6C9']}
-    //     })
-
-    var icon = createIcon({ // All options are optional
-        seed: 'randstring', // seed used to generate icon data, default: random
-        color: '#dfe', // to manually specify the icon color, default: random
-        bgcolor: '#aaa', // choose a different background color, default: white
-        size: 15, // width/height of the icon in blocks, default: 10
-        scale: 3 // width/height of each block in pixels, default: 5
-    });
-
+    
     function getBase64ImageFromSlice(slice) {
-
-console.log(slice)
-
-        base64Code = "iVBORw0KGgoAAAANSUhEUgAAAFUAAAA2CAYAAAC2ldWuAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAVaADAAQAAAABAAAANgAAAADTPCIuAAADXUlEQVR4Ae3ab0/aQBwH8G9ZdRhFNmEs2aJmBjUjUSFRt2XJnuzhXogvSR/7FvYW9kDizP7hRJkKGP4OHH8UKKXt7rqcaZYsQ7eTlt4ll19pz8v1kx/tcScgihAQAkJACAgBISAEhIAQEAJCQAjcTEDq9882NzeNftu6vZ18HYC3ypvrNHdtW49r75zjjQtUDrgCVaByEODQpchUgcpBgEOXIlMFKgcBDl2KTBWoHAQ4dCkyVaByEODQpchUgcpBgEOXIlN5oJLF5/Xt7e0Qh75d26Xs8/ninU4HW1tbLaJwSmqa1BNSP5Aa39jYSEqSpJNjUfoUkJLJpLlNomkaKG673TZjs9lEvV6nnxsEddcwjNe76ip+GPegGHf77N6dza5Q/3T7qqqCAjcaDRPZxO7KqOhBFPWHJD6ABvFotvr9FdXamB23Wi1Uq9VftdZEWQuiRIBLeohk8Shr5tp4I1SrVq/XM3ErlQoqBLqiTpoZnNMeowOvtalrjv8Z1Sql6zpqtRoocKFYQl4NIqPN4Dt5RLhpf/u/olqB6YuvXC4jl8uh3NQI7jTOSO244CXHDdUKfHFxgXw+b2ZvrhvEqfYEVWPK2mSojm8FlYnR7C2VSshmszi7HMehtoBz/T67PDTxVlGZGpnzolgsIp1OI9uawGFvETUy/x2WMhBUhkdfbBQ3k8kg05rEEcncmu5nlx0bB4rK1BguzdzTdgAJ9SkUB0/HbIFqxaVZe5zJ46u6QGYMs46citkKleHSX2xkTQLpmoTPvWU0dB+75IhoS1QmVygUkPp2jENlGke9BbLGcIddsnW0NSqVows6qVQK6VITe90oWSWz/xTM9qgsJekCTmL/AF+UME60OXbaltExa3aBQADP1lfxYiqHtZE9yFBtCUoH5RhUOliv14tYLIbVGRmvRt/BL9XpaduVa/3Pvx1G7/F4MD8/D7+/jImDOD4pi8jos3YY2tUYHIfKRh4KhUD21zCeSGCifon9XoRdGnh01Nf/d62xsTHzcRCbOkdU/gjJJj8VHI1KkWVZRjQaRTTUwZr8nsxkB7/x63hUCkufs0tLS1h+JOH5SHzgM4OhQKWwZBsdkUgEK7PjeDm6A6+k0NMDKUODyvTC4TBW5gIkY3cwgi47favxJ1G1cSyCOX+iAAAAAElFTkSuQmCC";
-        
-        // console.log("hi2", selection.count(), selection.firstObject().class())
-        // if (selection.count() == 1 && selection.firstObject().class() == "MSSliceLayer") {
-
-        //     var slice = selection.firstObject();
-        // var exportRequest = MSExportRequest.exportRequestsFromExportableLayer_inRect_useIDForName(
-        //     slice, slice.absoluteInfluenceRect(), false
-        // ).firstObject();
-        // var format = exportRequest.format();
-        // var exporter = MSExporter.exporterForRequest_colorSpace(exportRequest, NSColorSpace.sRGBColorSpace());
-        // var imageData = exporter.data();
-        // var base64Code = imageData.base64EncodedStringWithOptions(NSDataBase64EncodingEndLineWithLineFeed);
-
+        // From: https://github.com/Ashung/Automate-Sketch/blob/master/automate-sketch.sketchplugin/Contents/Sketch/Development/Copy_Slice_As_Base64.js
+        const exportRequest = MSExportRequest.exportRequestsFromExportableLayer_inRect_useIDForName(
+            slice, slice.absoluteInfluenceRect(), false
+        ).firstObject();
+        const exporter = MSExporter.exporterForRequest_colorSpace(exportRequest, NSColorSpace.sRGBColorSpace());
+        const imageData = exporter.data();
+        const base64Code = imageData.base64EncodedStringWithOptions(NSDataBase64EncodingEndLineWithLineFeed);
         return base64Code;
-        // var base64Preview = base64Code.substr(0, 8) + "..." + base64Code.substr(-8, 8);
-
-        //     switch (format + "") {
-        //         case "png":
-        //             base64Code = "data:image/png;base64," + base64Code;
-        //             break;
-        //         case "jpg":
-        //             base64Code = "data:image/jpeg;base64," + base64Code;
-        //             break;
-        //         case "tif":
-        //             base64Code = "data:image/tiff;base64," + base64Code;
-        //             break;
-        //         case "webp":
-        //             base64Code = "data:image/webp;base64," + base64Code;
-        //             break;
-        //         case "svg":
-        //             base64Code = "data:image/svg+xml;base64," + base64Code;
-        //             break;
-        //     }
-        //     console.log(base64Code);
-        // }
     }
-
 }
